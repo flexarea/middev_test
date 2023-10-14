d1cd468a7337c2c1607d638d857b4738dea1c66f
esdras <esdrasntuyenabo40@gmail.com>
2023-10-14 14:45:49 -0400

    added component folder and Form.tsx

--- api_test/src/App.tsx
+++ api_test/src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import { Form } from "./components/Form";
 
 function App() {
-  return <div className="App">api test</div>;
+  return (
+    <div className="App">
+      <h2>api test</h2>
+      <Form />
+    </div>
+  );
 }
 
 export default App;
---
+++ api_test/src/components/Form.tsx
@@ -0,0 +1,8 @@
+export const Form = () => {
+  return (
+    <div className="idk">
+      <label>Card Number</label>
+      <input type="text" />
+    </div>
+  );
+};
